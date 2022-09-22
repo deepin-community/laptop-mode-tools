e25ae10bc848f731947501db5e37b150305c1216
Deepin Developer <deepin-dev@deepin.org>
2022-09-22 17:44:51 +0800

    feat: Init commit
    init laptop-mode-tools
    
    Log:

---
+++ gui/lmt.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+""" Laptop Mode Tools """
+
+
+import sys
+from getpass import getuser
+from os import access, F_OK, geteuid, listdir, path, putenv
+from shutil import move
+from webbrowser import open_new_tab
+
+from PyQt5.QtWidgets import (QWidget, QMessageBox, QVBoxLayout, QHBoxLayout,
+        QGroupBox, QScrollArea, QCheckBox, QPushButton,
+        QApplication, QMainWindow, QDialogButtonBox,
+        QGraphicsDropShadowEffect, QShortcut)
+from PyQt5.QtGui import (QIcon, QColor)
+
+
+# This seems to be needed, atleast of Debian
+putenv('QT_X11_NO_MITSHM', "1")
+
+# constants
+COMMENT_IDENTIFIER = "#__COMMENT"
+CONTROL_IDENTIFIER = "CONTROL_"
+CONFIG_DIR = "/etc/laptop-mode/conf.d"
+WEBPAGE_URL = "http://github.com/rickysarraf/laptop-mode-tools"
+
+
+###############################################################################
+
+
+class Log():
+    def debug(self, string_to_log):
+        sys.stderr.write(string_to_log + "\n")
+
+    def msg(self, string_to_log):
+        sys.stdout.write(string_to_log + "\n")
+
+    def err(self, string_to_log):
+        sys.stderr.write(string_to_log + "\n")
+
+
+class MainWidget(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        self.configOptions, self.checkBoxList, self.configBool = {}, {}, None
+        # Check for root privileges
+        if geteuid() != 0:
+            msg = ("{} is not root. You need to run with root privileges\n"
+                   "Please use kdesudo, gksu or sudo/sux.").format(getuser())
+            QMessageBox.critical(self, __doc__ + "- Error", msg)
+            sys.exit(1)
+        else:
+            msg = "This tool is running with root privileges."
+            QMessageBox.warning(self, __doc__ + "- Warning", msg)
+        # title, icon and sizes
+        self.setWindowTitle(__doc__)
+        self.setMinimumSize(400, 400)
+        self.setMaximumSize(2048, 2048)
+        self.resize(600, 600)
+        self.setWindowIcon(QIcon.fromTheme("preferences-system"))
+        self.menuBar().addMenu("&File").addAction("Exit", exit)
+        QShortcut("Ctrl+q", self, activated=lambda: self.close())
+        # main group
+        main_group = QGroupBox("Module configuration")
+        self.setCentralWidget(main_group)
+        self.layout = QVBoxLayout(main_group)
+        # scrollarea widgets
+        self.scrollArea, self.window = QScrollArea(), QWidget()
+        self.layout.addWidget(self.scrollArea)
+        self.vbox = QVBoxLayout(self.window)
+        # Graphic effect
+        glow = QGraphicsDropShadowEffect(self)
+        glow.setOffset(0)
+        glow.setBlurRadius(99)
+        glow.setColor(QColor(99, 255, 255))
+        self.scrollArea.setGraphicsEffect(glow)
+        glow.setEnabled(True)
+        # config loading stuff
+        self.findConfig(CONFIG_DIR)
+        for eachOption in tuple(self.configOptions.keys()):
+
+            self.readConfig(eachOption, self.configOptions)
+            self.subLayout = QHBoxLayout()
+
+            self.checkBoxName = "checkBox_" + eachOption
+            checkBoxList = QCheckBox(self.checkBoxName, self)
+            self.checkBoxList[self.checkBoxName] = checkBoxList
+            checkBoxList.setObjectName(self.checkBoxName)
+            checkBoxList.setText("Enable module {}".format(eachOption))
+
+            if self.tooltip is not '':
+                checkBoxList.setToolTip(self.tooltip)
+            else:
+                tooltip = "Configuration settings for {}".format(eachOption)
+                checkBoxList.setToolTip(tooltip)
+
+            if self.configBool:
+                checkBoxList.setChecked(True)
+
+            self.subLayout.addWidget(checkBoxList)
+            self.vbox.addLayout(self.subLayout)
+        self.scrollArea.setWidget(self.window)
+
+        # Bottom Buttons Bar
+        self.pushButtonSleep = QPushButton("Sleep")
+        self.pushButtonSleep.setToolTip("Trigger Suspend to RAM aka Sleep")
+        self.pushButtonSleep.clicked.connect(self.sleep)
+        self.pushButtonHibernate = QPushButton("Hibernate")
+        self.pushButtonHibernate.setToolTip("Trigger Suspend to Disk Hibernate")
+        self.pushButtonHibernate.clicked.connect(self.hibernate)
+        self.buttonBox = QDialogButtonBox()
+        self.buttonBox.setStandardButtons(
+            QDialogButtonBox.Ok | QDialogButtonBox.Close |
+            QDialogButtonBox.Help)
+        self.buttonBox.addButton(self.pushButtonHibernate,
+                                 QDialogButtonBox.ActionRole)
+        self.buttonBox.addButton(self.pushButtonSleep,
+                                 QDialogButtonBox.ActionRole)
+        self.layout.addWidget(self.buttonBox)
+        self.buttonBox.rejected.connect(exit)
+        self.buttonBox.accepted.connect(self.writeConfig)
+        self.buttonBox.helpRequested.connect(lambda: open_new_tab(WEBPAGE_URL))
+
+    def closeEvent(self, event):
+        ' Ask to Quit '
+        the_conditional_is_true = QMessageBox.question(
+            self, __doc__.title(), 'Quit ?.', QMessageBox.Yes | QMessageBox.No,
+            QMessageBox.No) == QMessageBox.Yes
+        event.accept() if the_conditional_is_true else event.ignore()
+
+    def sleep(self):
+        """Method to make the computer Sleep."""
+        try:
+            sysfsFP = open("/sys/power/state", 'w')
+        except:
+            log.err("Couldn't open kernel interface")
+            return False
+        else:
+            try:
+                sysfsFP.write("mem")
+            except:
+                log.err("Couldn't write to kernel interface")
+                return False
+
+    def hibernate(self):
+        """Method to make the computer Hibernate."""
+        try:
+            sysfsFP = open("/sys/power/state", 'w')
+        except:
+            log.err("Couldn't open kernel interface")
+            return False
+        else:
+            try:
+                sysfsFP.write("disk")
+            except:
+                log.err("Couldn't write to kernel interface")
+                return False
+
+    def writeConfig(self):
+        """Method to get a configuration for the App."""
+        finalResult = True
+        for eachWriteOption in tuple(self.configOptions.keys()):
+            checkBoxName = "checkBox_" + eachWriteOption
+            if self.checkBoxList[checkBoxName].isChecked() is True:
+                val = 1
+            else:
+                val = 0
+            ret = self.populateValues(self.configOptions[eachWriteOption], val)
+
+            if ret is False:
+                log.debug("Couldn't apply setting for %s" % checkBoxName)
+                finalResult = False
+
+        if finalResult is False:
+            QMessageBox.critical(self, "Error",
+                                       "Couldn't apply all requested settings")
+        else:
+            QMessageBox.information(self, "Success",
+                                          "Applied all requested settings")
+
+    def populateValues(self, _path, value):
+        """Method to populate values from a file path."""
+        try:
+            readHandle = open(_path, 'r')
+            writeHandle = open(_path + ".tmp", 'w')
+            for line in readHandle.readlines():
+                if line.startswith(CONTROL_IDENTIFIER):
+                    newline = line.split("=")[0] + "=" + str(value)
+                    writeHandle.write(newline)
+                    # You need this newline, otherwise the next line gets
+                    # overlapped here
+                    writeHandle.write("\n")
+                else:
+                    writeHandle.write(line)
+            readHandle.close()
+            writeHandle.close()
+            move(_path + ".tmp", _path)
+            return True
+        except:
+            log.debug("Failed in populateValues() when operating on %s" % _path)
+            return False
+
+    def findConfig(self, configDir):
+        """Take a configDir and find the configuration for the App."""
+        if configDir is None:
+            return False
+
+        # TODO: Do we need to take care of the vendor specific overrides ???
+        for configFile in listdir(configDir):
+            if access(path.join(configDir, configFile), F_OK) is True:
+                fn = path.join(configDir, configFile)
+                self.configOptions[configFile.split(".")[0]] = fn
+            else:
+                pass
+
+    def readConfig(self, key, configOptionsDict):
+        """Take a key and dict and read the configurations for the App."""
+        self.tooltip = ''
+
+        if key is None or configOptionsDict is None:
+            return False
+
+        try:
+            fileHandle = open(configOptionsDict[key], 'r')
+        except:
+            return False
+
+        for line in fileHandle.readlines():
+            if line.startswith(COMMENT_IDENTIFIER):
+                self.tooltip = self.tooltip + line.lstrip(COMMENT_IDENTIFIER)
+            elif line.startswith(CONTROL_IDENTIFIER):
+                boolValue = line.split("=")[1]
+                # Bloody boolValue could inherit the '\n' new line
+                boolValue = boolValue.rstrip("\n")
+
+                if boolValue == str(1) or "\"auto\"" in boolValue:
+                    self.configBool = True
+                else:
+                    self.configBool = False
+
+        # This will ensure that even if we don't read any string, tooltip
+        # doesn't fail
+        self.tooltip = self.tooltip + ''
+
+
+###############################################################################
+
+
+if __name__ == "__main__":
+    log = Log()
+    application = QApplication(sys.argv)
+    application.setApplicationName(__doc__.strip().lower())
+    application.setOrganizationName(__doc__.strip().title())
+    application.setOrganizationDomain(__doc__.strip().title())
+    application.setWindowIcon(QIcon.fromTheme("preferences-system"))
+    window = MainWidget()
+    window.show()
+    window.raise_()
+    sys.exit(application.exec_())
---
+++ laptop-mode-tools.spec
@@ -0,0 +1,217 @@
+# Authority: dag
+# Upstream: Ritesh Raj Sarraf <rrs@researchut.com>
+
+Summary: Tools for power savings based on battery/AC status
+Name: laptop-mode-tools
+Version: 1.74
+Release: 1
+License: GPL
+Group: System Environment/Base
+URL: http://github.com/rickysarraf/laptop-mode-tools
+Vendor: Laptop Mode Tools Developers
+Distribution: RPM Based distributions
+Packager: Ritesh Raj Sarraf <rrs@researchut.com>
+Source: https://github.com/rickysarraf/laptop-mode-tools/releases/download/%{version}/laptop-mode-tools_%{version}.tar.gz
+BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+
+BuildArch: noarch
+
+%description
+Laptop mode is a Linux kernel feature that allows your laptop to save
+considerable power, by allowing the hard drive to spin down for longer
+periods of time. This package contains the userland scripts that are
+needed to enable laptop mode. It includes support for automatically
+enabling laptop mode when the computer is working on batteries. In
+addition, it provides a set of modules which allow you to apply
+various other power savings.
+
+%prep
+%setup -n %{name}_%{version}
+
+%build
+
+%{__rm} -rf %{buildroot}
+
+DESTDIR=%{buildroot} INIT_D="" MAN_D=%{_mandir} INSTALL=install ./install.sh
+
+# Work around bug in installer. It installs the script in the wrong location if INIT_D="".
+rm %{buildroot}/etc/init.d/laptop-mode
+
+%{__mkdir_p} -m0755 %{buildroot}%{_initrddir}
+%{__install} -Dp -m755 etc/init.d/laptop-mode %{buildroot}%{_initrddir}
+
+%clean
+%{__rm} -rf %{buildroot}
+
+%preun
+if [ $1 -eq 0 ]; then
+	/sbin/service laptop-mode stop &>/dev/null || :
+	/sbin/chkconfig --del laptop-mode
+fi
+
+%post
+/sbin/chkconfig --add laptop-mode
+/sbin/service laptop-mode start &>/dev/null || :
+/sbin/service acpid restart &>/dev/null || :
+
+%postun
+/sbin/service laptop-mode condrestart &>/dev/null || :
+
+%files
+%defattr(-, root, root, 0755)
+
+%doc COPYING Documentation/*.txt README.md
+%docdir %{_mandir}
+%doc %{_mandir}/man8/laptop-mode.conf.8
+%doc %{_mandir}/man8/laptop_mode.8
+%doc %{_mandir}/man8/lm-profiler.8
+%doc %{_mandir}/man8/lm-profiler.conf.8
+%doc %{_mandir}/man8/lm-syslog-setup.8
+%config %{_sysconfdir}/acpi/actions/lm_*.sh
+%config %{_sysconfdir}/acpi/events/lm_*
+%config(noreplace) %{_sysconfdir}/laptop-mode/
+%config %{_initrddir}/laptop-mode
+/lib/udev/lmt-udev
+/lib/systemd/system/laptop-mode.service
+/lib/systemd/system/laptop-mode.timer
+/lib/systemd/system/lmt-poll.service
+/lib/udev/rules.d/99-laptop-mode.rules
+
+%{_sysconfdir}/apm/event.d/*
+%{_sysconfdir}/power/scripts.d/*
+%{_sysconfdir}/power/event.d/*
+#%{_usr}/sbin/*
+%{_sbindir}/*
+%{_usr}/share/applications/laptop-mode-tools.desktop
+%{_usr}/share/laptop-mode-tools/lmt.py
+%{_usr}/share/laptop-mode-tools/modules/*
+%{_usr}/share/laptop-mode-tools/module-helpers/*
+%{_usr}/share/pixmaps/laptop-mode-tools.svg
+%{_usr}/share/polkit-1/actions/org.linux.lmt.gui.policy
+%{_usr}/lib/pm-utils/sleep.d/*
+%{_usr}/lib/tmpfiles.d/laptop-mode.conf
+
+
+%dir %{_sysconfdir}/acpi/events
+%dir %{_sysconfdir}/acpi/actions
+%dir %{_usr}/lib/pm-utils/sleep.d
+%dir %{_usr}/lib/tmpfiles.d
+%dir %{_usr}/share/applications
+%dir %{_usr}/share/pixmaps
+%dir %{_usr}/share/laptop-mode-tools/modules
+%dir %{_usr}/share/laptop-mode-tools/module-helpers
+%dir %{_sysconfdir}/apm/event.d
+%dir %{_sysconfdir}/power/scripts.d
+%dir %{_sysconfdir}/power/event.d
+
+
+%changelog
+* Sat Jul 18 2020 Ritesh Raj Sarraf <rrs@researchut.com> - 1.74-1
+- Updated to release 1.74
+
+* Sat Jan 11 2020 Ritesh Raj Sarraf <rrs@researchut.com> - 1.73.1-1
+- Updated to release 1.73.1
+
+* Sat Jan 11 2020 Ritesh Raj Sarraf <rrs@researchut.com> - 1.73-1
+- Updated to release 1.73
+
+* Sat Feb  3 2018 Ritesh Raj Sarraf <rrs@researchut.com> - 1.72.2-1
+- Updated to release 1.72.2
+
+* Thu Feb  1 2018 Ritesh Raj Sarraf <rrs@researchut.com> - 1.72.1-1
+- Updated to release 1.72.1
+
+* Thu Jan 12 2017 Ritesh Raj Sarraf <rrs@researchut.com> - 1.71-1
+- Updated to release 1.71
+
+* Sat Sep 24 2016 Ritesh Raj Sarraf <rrs@researchut.com> - 1.70-1
+- Updated to release 1.70
+
+* Sun Mar 27 2016 Ritesh Raj Sarraf <rrs@researchut.com> - 1.69.2-1
+- Updated to release 1.69.2
+
+* Tue Mar 15 2016 Ritesh Raj Sarraf <rrs@researchut.com> - 1.69.1-1
+- Updated to release 1.69.1
+
+* Mon Mar  7 2016 Ritesh Raj Sarraf <rrs@researchut.com> - 1.69-1
+- Updated to release 1.69
+
+* Sun Sep 27 2015 Ritesh Raj Sarraf <rrs@researchut.com> - 1.68.1-1
+- Updated to release 1.68.1
+
+* Thu Aug 27 2015 Ritesh Raj Sarraf <rrs@researchut.com> - 1.68-1
+- Updated to release 1.68.
+
+* Thu Jul  2 2015 Ritesh Raj Sarraf <rrs@researchut.com> - 1.67-1
+- Updated to release 1.67.
+
+* Fri Sep 26 2014 Ritesh Raj Sarraf <rrs@researchut.com> - 1.66-1
+- Updated to release 1.66.
+
+* Wed Jun 18 2014 Ritesh Raj Sarraf <rrs@researchut.com> - 1.65-1
+- Updated to release 1.65.
+
+* Sun Sep  1 2013 Ritesh Raj Sarraf <rrs@researchut.com> - 1.64-1
+- Updated to release 1.64.
+
+* Sat Mar 23 2013 Ritesh Raj Sarraf <rrs@researchut.com> - 1.63-1
+- Updated to release 1.63.
+
+* Tue Oct 30 2012 Ritesh Raj Sarraf <rrs@researchut.com> - 1.62-1
+- Updated to release 1.62.
+
+* Thu May 17 2012 Ritesh Raj Sarraf <rrs@researchut.com> - 1.61-1
+- Updated to release 1.61.
+
+* Fri Oct 14 2011 Ritesh Raj Sarraf <rrs@researchut.com> - 1.60-1
+- Updated to release 1.60.
+
+* Mon Aug  8 2011 Ritesh Raj Sarraf <rrs@researchut.com> - 1.59-1
+- Updated to release 1.59.
+
+* Sat Jul 23 2011 Ritesh Raj Sarraf <rrs@researchut.com> - 1.58-1
+- Updated to release 1.58.
+
+* Sun Feb 27 2011 Ritesh Raj Sarraf <rrs@researchut.com> - 1.57-1
+- Updated to release 1.57.
+
+* Mon Feb 14 2011 Ritesh Raj Sarraf <rrs@researchut.com> - 1.56-1
+- Updated to release 1.56.
+
+* Wed May 26 2010 Ritesh Raj Sarraf <rrs@researchut.com> - 1.55-1
+- Updated to release 1.55.
+
+* Wed Mar 10 2010 Ritesh Raj Sarraf <rrs@researchut.com> - 1.54-1
+- Updated to release 1.54.
+
+* Sat Jan  2 2010 Ritesh Raj Sarraf <rrs@researchut.com> - 1.53-1
+- Updated to release 1.53.
+
+* Thu Oct  8 2009 Ritesh Raj Sarraf <rrs@researchut.com> - 1.52-1
+- Updated to release 1.52.
+
+* Tue Sep  1 2009 Ritesh Raj Sarraf <rrs@researchut.com> - 1.51-1
+- Updated to release 1.51.
+
+* Fri Jul 24 2009 Ritesh Raj Sarraf <rrs@researchut.com> - 1.50-1
+- Updated to release 1.50.
+
+* Sun Sep 07 2008 Bart Samwel <bart@samwel.tk> - 1.45-1
+- Updated to release 1.45.
+
+* Mon May 28 2007 Bart Samwel <bart@samwel.tk> - 1.34-1
+- Updated to release 1.34.
+- Added some files from upstream that were left out in earlier packages, such as lm-profiler.
+- Restart acpid after %{__install} -Dping.
+
+* Sun Oct 08 2006 Dag Wieers <dag@wieers.com> - 1.32-1
+- Updated to release 1.32.
+
+* Sun Apr 16 2006 Dag Wieers <dag@wieers.com> - 1.31-1
+- Updated to release 1.31.
+
+* Sat Apr 15 2006 Dag Wieers <dag@wieers.com> - 1.30-1
+- Updated to release 1.30.
+
+* Sun Apr 10 2005 Dag Wieers <dag@wieers.com> - 1.05-1
+- Initial package. (using DAR)
